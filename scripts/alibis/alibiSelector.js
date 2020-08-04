b8f7363934b593ccd6b377a17fba5733ff367a28
Austin Ring <aring97@gmail.com>
2020-08-04 09:49:14 -0500

    added commits

--- scripts/alibis/alibiSelector.js
+++ scripts/alibis/alibiSelector.js
@@ -1,13 +1,15 @@
 import {getCriminals, useCriminals} from "../criminals/criminalsDataProvider.js"
 import { alibiHTML } from "./alibiHTML.js"
 
-//get a reference to the DOM elements that will hold the alibi button
+//get a reference to the DOM elements that will hold the alibi
 const contentTarget=document.querySelector(".criminalsContainer")
 const target=document.querySelector(".alibiContainer")
-//const target=document.querySelector(".criminalContainer")
+//event listener looking for a click
 contentTarget.addEventListener("click",(boxClicked)=>{
+    //get the criminals from the api
     getCriminals()
     .then(()=>{const criminalList=useCriminals()
+        //finding the criminal that matches the parsedInt 
         const matchingCriminal = criminalList.filter((currentCriminal)=>{
             const targetId=parseInt(boxClicked.target.id)
             return targetId===currentCriminal.id
